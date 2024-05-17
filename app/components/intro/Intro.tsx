32edaeffce06ae5b8b1367c9445b41b0f72070ee
Ibrahim Ahmed <ahmed1768476@gmail.com>
2024-05-17 13:11:27 +0100

    fix text size

--- app/components/intro/Intro.tsx
+++ app/components/intro/Intro.tsx
@@ -2,12 +2,12 @@ import React from "react";
 
 function Intro() {
   return (
-    <section className="w-full h-[80vh] bg-homeIntroBg bg-cover bg-center bg-no-repeat xl:h-screen">
-      <div className="w-full h-full pt-60 px-4 flex flex-col items-center bg-gradient-to-t from-gray-700 to-transparent">
+    <section className="w-full h-[90vh] bg-homeIntroBg bg-cover bg-center bg-no-repeat xl:h-screen">
+      <div className="w-full h-full pt-60 px-4 flex flex-col items-center bg-gradient-to-t from-[#3a3a3a] to-transparent">
         <h1 className="font-yeseva text-white font-bold text-7xl sm:text-8xl md:text-9xl">
           Welcome!
         </h1>
-        <p className="font-yeseva text-white text-base font-bold text-center my-8">
+        <p className="font-yeseva text-white text-base font-bold text-center my-8 md:text-4xl">
           Come enjoy the lorem ipsum dolor sit amet, consectetur
           <br className="hidden md:block" /> adipisicing elit, sed do eiusmod
         </p>
